d6163db12ff24ccca0d1dbc2b2e500b58844c66f
Roberto Visconti <robertovisconti93@gmail.com>
2026-07-08 16:33:36 +0200

    add errors handler

---
+++ src/main/java/robertovisconti/be_u5_w2_d3/exceptions/ErrorsHandler.java
@@ -0,0 +1,33 @@
+package robertovisconti.be_u5_w2_d3.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import robertovisconti.be_u5_w2_d3.payloads.ErrorsPayload;
+
+import java.time.LocalDateTime;
+
+@RestControllerAdvice
+public class ErrorsHandler {
+
+    @ExceptionHandler(BadRequestException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorsPayload handleBadRequest(BadRequestException ex) {
+        return new ErrorsPayload(ex.getMessage(), LocalDateTime.now());
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorsPayload handleNotFound(NotFoundException ex) {
+        return new ErrorsPayload(ex.getMessage(), LocalDateTime.now());
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorsPayload handleGenericException(Exception ex) {
+        ex.printStackTrace();
+        return new ErrorsPayload("Errore interno al server, risolveremo il prima possibile.", LocalDateTime.now());
+    }
+
+}
---
+++ src/main/java/robertovisconti/be_u5_w2_d3/payloads/ErrorsPayload.java
@@ -0,0 +1,13 @@
+package robertovisconti.be_u5_w2_d3.payloads;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+public class ErrorsPayload {
+    private String message;
+    private LocalDateTime timestamp;
+}
